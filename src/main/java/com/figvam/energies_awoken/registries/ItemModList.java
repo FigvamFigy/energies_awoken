5adbab63441d670773c2d37bd3fcb6a2f846fd53
FigvamFigy <looyloop123@gmail.com>
2020-03-25 14:30:21 -0700

    Adds RegistryEventHandler and mod list items

---
+++ src/main/java/com/figvam/energies_awoken/registries/ItemModList.java
@@ -0,0 +1,14 @@
+package com.figvam.energies_awoken.registries;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber
+public class ItemModList {
+
+
+    public static final Item[] ITEMS = {
+
+    };
+
+}
---
+++ src/main/java/com/figvam/energies_awoken/registries/TileEntityModList.java
@@ -0,0 +1,16 @@
+package com.figvam.energies_awoken.registries;
+
+import net.minecraft.tileentity.TileEntity;
+
+public class TileEntityModList {
+
+    public static final TileEntity[] TILE_ENTITIES = {
+
+
+    };
+
+
+
+
+
+}
