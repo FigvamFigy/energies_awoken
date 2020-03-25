21be251f9eaea3f6030617e859ddcb7ee2db8564
FigvamFigy <looyloop123@gmail.com>
2020-03-25 14:42:49 -0700

    Adds AcceptedItemsInBreakdown

---
+++ src/main/java/com/figvam/energies_awoken/util/AcceptedItemsInBreakdown.java
@@ -0,0 +1,15 @@
+package com.figvam.energies_awoken.util;
+
+import net.minecraft.item.Item;
+
+public class AcceptedItemsInBreakdown {
+
+
+    public static final Item[] ACCEPTED_ITEMS = {
+
+
+    };
+
+
+
+}
