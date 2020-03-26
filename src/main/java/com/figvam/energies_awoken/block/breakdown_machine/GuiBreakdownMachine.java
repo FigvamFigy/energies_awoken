9a6e97ff6b40cef92d185de56fa7e0719778243f
FigvamFigy <looyloop123@gmail.com>
2020-03-26 14:46:55 -0700

    Adds "breakdown Machine" text on top of gui

--- src/main/java/com/figvam/energies_awoken/block/breakdown_machine/GuiBreakdownMachine.java
+++ src/main/java/com/figvam/energies_awoken/block/breakdown_machine/GuiBreakdownMachine.java
@@ -1,9 +1,12 @@
 package com.figvam.energies_awoken.block.breakdown_machine;
 
 import com.figvam.energies_awoken.ModDetailReference;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.client.FMLClientHandler;
 
 public class GuiBreakdownMachine extends GuiContainer {
 
@@ -29,6 +32,20 @@ public class GuiBreakdownMachine extends GuiContainer {
         //ySize = 166;
 
         this.drawTexturedModalRect(middleX,middleY,0,0,this.xSize,ySize);
+
+
+        String str = "Breakdown Machine";
+        int strWidth = fontRenderer.getStringWidth(str);
+
+        int strX = (width / 2) - (strWidth / 2);
+        int strY = (height / 2) - (20 / 2) - 65;
+
+
+        FontRenderer fontRenderer = FMLClientHandler.instance().getClient().fontRenderer;
+        fontRenderer.drawString(str, strX, strY, 4210752);
+
+
+
 
     }
 }
