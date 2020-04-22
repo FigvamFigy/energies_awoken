bdd669280a9f3642cd939377490f9dfc3ef593b9
FigvamFigy <looyloop123@gmail.com>
2020-04-22 16:11:20 -0700

    Adds fortune capability with geode ores

--- src/main/java/com/figvam/energies_awoken/block/block_geode/BlockPurpleGeodeOre.java
+++ src/main/java/com/figvam/energies_awoken/block/block_geode/BlockPurpleGeodeOre.java
@@ -1,8 +1,16 @@
 package com.figvam.energies_awoken.block.block_geode;
 
+import com.figvam.energies_awoken.item.item.geode.ItemBlueGeode;
+import com.figvam.energies_awoken.item.item.geode.ItemPurpleGeode;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+
+import java.util.Random;
 
 
 public class BlockPurpleGeodeOre extends Block {
@@ -13,6 +21,7 @@ public class BlockPurpleGeodeOre extends Block {
     private BlockPurpleGeodeOre(){
         super(Material.ROCK);
         setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+        setHardness(5f);
 
         setUnlocalizedName("purple_geode_ore");
         setRegistryName("purple_geode_ore");
@@ -25,5 +34,46 @@ public class BlockPurpleGeodeOre extends Block {
             instance = new BlockPurpleGeodeOre();
         }
         return instance;
+    }
+    @Override
+    public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+        return ItemPurpleGeode.getInstance();
+    }
+
+
+    //Without fortune
+    @Override
+    public int quantityDropped(IBlockState blockState, int fortune, Random random){
+        return quantityDroppedWithBonus(fortune,random);
+    }
+
+    //With Fortune
+    @Override
+    public int quantityDroppedWithBonus(int fortuneLevel, Random random){
+
+        if (fortuneLevel > 0 && Item.getItemFromBlock(this) != this.getItemDropped((IBlockState)this.getBlockState().getValidStates().iterator().next(), random, fortuneLevel))
+        {
+            int i = random.nextInt(fortuneLevel + 2) - 1;
+
+            if (i < 0)
+            {
+                i = 0;
+            }
+
+            return this.quantityDropped(random) * (i + 1);
+        }
+        else
+        {
+            return this.quantityDropped(random);
+        }
+
+    }
+
+
+    //EXP
+    @Override
+    public int getExpDrop(IBlockState state, IBlockAccess world, BlockPos pos, int fortune)
+    {
+        return 2;
     }
 }
--- src/main/java/com/figvam/energies_awoken/block/block_geode/BlockRedGeodeOre.java
+++ src/main/java/com/figvam/energies_awoken/block/block_geode/BlockRedGeodeOre.java
@@ -1,8 +1,16 @@
 package com.figvam.energies_awoken.block.block_geode;
 
+import com.figvam.energies_awoken.item.item.geode.ItemBlueGeode;
+import com.figvam.energies_awoken.item.item.geode.ItemRedGeode;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+
+import java.util.Random;
 
 public class BlockRedGeodeOre extends Block {
 
@@ -12,6 +20,7 @@ public class BlockRedGeodeOre extends Block {
     private BlockRedGeodeOre(){
         super(Material.ROCK);
         setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+        setHardness(5f);
 
         setUnlocalizedName("red_geode_ore");
         setRegistryName("red_geode_ore");
@@ -23,4 +32,47 @@ public class BlockRedGeodeOre extends Block {
         }
         return instance;
     }
+
+    @Override
+    public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+        return ItemRedGeode.getInstance();
+    }
+
+
+    //Without fortune
+    @Override
+    public int quantityDropped(IBlockState blockState, int fortune, Random random){
+        return quantityDroppedWithBonus(fortune,random);
+    }
+
+    //With Fortune
+    @Override
+    public int quantityDroppedWithBonus(int fortuneLevel, Random random){
+
+        if (fortuneLevel > 0 && Item.getItemFromBlock(this) != this.getItemDropped((IBlockState)this.getBlockState().getValidStates().iterator().next(), random, fortuneLevel))
+        {
+            int i = random.nextInt(fortuneLevel + 2) - 1;
+
+            if (i < 0)
+            {
+                i = 0;
+            }
+
+            return this.quantityDropped(random) * (i + 1);
+        }
+        else
+        {
+            return this.quantityDropped(random);
+        }
+
+    }
+
+
+    //EXP
+    @Override
+    public int getExpDrop(IBlockState state, IBlockAccess world, BlockPos pos, int fortune)
+    {
+        return 2;
+    }
+
 }
