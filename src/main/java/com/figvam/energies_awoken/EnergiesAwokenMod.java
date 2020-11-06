af33e2bdbe06c0346804378979f55fbb3afb2c31
FigvamFigy <looyloop123@gmail.com>
2020-11-06 15:46:52 -0800

    Sets up registry

--- src/main/java/com/figvam/energies_awoken/EnergiesAwokenMod.java
+++ src/main/java/com/figvam/energies_awoken/EnergiesAwokenMod.java
@@ -1,5 +1,6 @@
 package com.figvam.energies_awoken;
 
+import com.figvam.energies_awoken.common.registries.Registry;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
@@ -19,13 +20,18 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("energies_awoken")
+@Mod(ModDetails.MOD_ID)
 public class EnergiesAwokenMod {
 
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
     public EnergiesAwokenMod() {
+
+        Registry.getBlockDeferredRegister().register(FMLJavaModLoadingContext.get().getModEventBus());
+        Registry.getItemDeferredRegister().register(FMLJavaModLoadingContext.get().getModEventBus());
+
+
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
---
+++ src/main/java/com/figvam/energies_awoken/ModDetails.java
@@ -0,0 +1,8 @@
+package com.figvam.energies_awoken;
+
+public class ModDetails {
+
+
+    public static final String MOD_ID = "energies_awoken";
+
+}
